1f34caea218546a6f9f0895f17c49c6baa7881bb
James Kiesel <james.kiesel@gmail.com>
2020-04-29 21:41:11 +1200

    Add loading animation

--- src/components/deck.js
+++ src/components/deck.js
@@ -3,13 +3,11 @@ import Card from "../components/card"
 import "../styles/deck.scss"
 
 const Deck = ({ type, names, CardClass = Card }) => (
-  <>
-    <div className="deck-cards">
-      {names.map((name, index) => (
-        <CardClass key={`${type}-${name}`} type={type} name={name} index={index} />
-      ))}
-    </div>
-  </>
+  <div className="deck">
+    {names.map((name, index) => (
+      <CardClass key={`${type}-${name}`} type={type} name={name} index={index} />
+    ))}
+  </div>
 )
 
 export default Deck
--- src/components/loading.js
+++ src/components/loading.js
@@ -3,7 +3,12 @@ import React from "react"
 import "../styles/loading.scss"
 
 const Loading = () => (
-  <div className="loading">Loading...</div>
+  <div className="loading">
+    <div className="loading-spinner">
+      <div className="loading-spinner-dot" />
+      <div className="loading-spinner-dot" />
+    </div>
+  </div>
 )
 
 export default Loading

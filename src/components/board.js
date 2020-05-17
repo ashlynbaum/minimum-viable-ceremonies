ae6dd72989bf255eb57a6b517dbe36a30cc52441
James <james.kiesel@gmail.com>
2020-05-17 19:49:21 +1200

    Rearrange board

--- src/components/board.js
+++ src/components/board.js
@@ -26,43 +26,42 @@ const Board = () => {
               <div className="sidebar-subtitle">Ceremonies</div>
               <Cadence className="flex-grow" id="undecided" />
             </div>
-
-            <div style={{flexBasis: "60%"}} className="flex flex-row">
-              <div className="flex flex-col">
-                <div className="sidebar-subtitle">Cadences</div>
-                <div className="flex flex-row">
-                  <div style={{flexBasis: "33.33%"}} className="flex flex-col">
-                    <Cadence id="daily" />
-                    <Cadence id="quarterly" />
+            <div style={{flexBasis: "80%"}} className="flex flex-col">
+              <div className="flex flex-row">
+                <div className="flex flex-col">
+                  <div className="sidebar-subtitle">Cadences</div>
+                  <div className="flex flex-row">
+                    <div style={{flexBasis: "33.33%"}} className="flex flex-col">
+                      <Cadence id="daily" />
+                      <Cadence id="quarterly" />
+                    </div>
+                    <div style={{flexBasis: "33.33%"}} className="flex flex-col">
+                      <Cadence id="weekly" />
+                      <Cadence id="halfyearly" />
+                    </div>
+                    <div style={{flexBasis: "33.33%"}} className="flex flex-col">
+                      <Cadence id="monthly" />
+                      <Cadence id="yearly" />
+                    </div>
                   </div>
-                  <div style={{flexBasis: "33.33%"}} className="flex flex-col">
-                    <Cadence id="weekly" />
-                    <Cadence id="halfyearly" />
-                  </div>
-                  <div style={{flexBasis: "33.33%"}} className="flex flex-col">
-                    <Cadence id="monthly" />
-                    <Cadence id="yearly" />
-                  </div>
+                </div>
+                <div style={{flexBasis: "20%"}} className="flex flex-col">
+                  <div className="sidebar-subtitle">The Void</div>
+                  <Cadence id="void" void={true} />
                 </div>
               </div>
-            </div>
-            <div style={{flexBasis: "20%"}} className="flex flex-col">
-              <div className="sidebar-subtitle">The Void</div>
-              <Cadence id="void" void={true} />
-            </div>
-          </div>
-          <div>
-            <div className="flex flex-col">
-              {[...Array(parseInt(weekCount))].map((_, index) => <div key={index}>
-                <div className="sidebar-subtitle">Sprint week {index}</div>
-                <div className="flex flex-row justify-around">
-                  <Cadence basis={5} id={`monday-${index+1}`} />
-                  <Cadence basis={5} id={`tuesday-${index+1}`} />
-                  <Cadence basis={5} id={`wednesday-${index+1}`} />
-                  <Cadence basis={5} id={`thursday-${index+1}`} />
-                  <Cadence basis={5} id={`friday-${index+1}`} />
-                </div>
-              </div>)}
+              <div className="flex flex-col">
+                {[...Array(parseInt(weekCount))].map((_, index) => <div key={index}>
+                  <div className="sidebar-subtitle">Sprint week {index}</div>
+                  <div className="flex flex-row justify-around">
+                    <Cadence basis={5} id={`monday-${index+1}`} />
+                    <Cadence basis={5} id={`tuesday-${index+1}`} />
+                    <Cadence basis={5} id={`wednesday-${index+1}`} />
+                    <Cadence basis={5} id={`thursday-${index+1}`} />
+                    <Cadence basis={5} id={`friday-${index+1}`} />
+                  </div>
+                </div>)}
+              </div>
             </div>
           </div>
         </div>

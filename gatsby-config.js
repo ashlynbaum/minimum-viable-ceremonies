14ae4f136f9926f90f85603db9ab8722b2c01304
James <james.kiesel@gmail.com>
2020-05-18 18:27:48 +1200

    Update README

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Minimum Viable Ceremonies`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Meetings suck - let's have less of them.`,
     author: `@gdpelican`,
   },
   plugins: [

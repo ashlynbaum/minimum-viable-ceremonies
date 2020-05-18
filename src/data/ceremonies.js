a3e75faac0c9eaeb1f919d25880c992fa909936a
James <james.kiesel@gmail.com>
2020-05-18 14:12:43 +1200

    Add rest of ceremonies

--- src/data/ceremonies.js
+++ src/data/ceremonies.js
@@ -1,5 +1,5 @@
 export default [
-  "checkin", "cleanup", "demo", "design_review", "documentation",
+  "checkin", "cleanup", "demo", "designReview", "documentation",
   "estimation", "growth", "hackathon", "horizon", "insight", "planning",
   "process", "retreat", "retrospective", "review", "roadmap", "social",
   "solution", "spec", "standdown", "standup", "sync", "techdebt"

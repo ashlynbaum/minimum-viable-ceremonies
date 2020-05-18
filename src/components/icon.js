fa614198cd16505cbe5134e168f703579b27a0f9
James <james.kiesel@gmail.com>
2020-05-18 13:50:18 +1200

    Finish fixing paths (?)

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Icon = ({ icon, size = 20 }) => {
-  const Svg = require(`../../images/icons/${icon}.svg`)
+  const Svg = require(`../images/icons/${icon}.svg`)
   return <Svg width={size} height={size} />
 }
 

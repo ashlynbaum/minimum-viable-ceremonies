a9a51e19284fc60aa169564c6d2969797ddaf6af
James <james.kiesel@gmail.com>
2020-05-20 10:15:23 +1200

    Another attempt to fix flashing 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,18 +3,13 @@ import React from "react"
 import Loading from "../components/loading"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { window } from "browser-monads"
 
 const NotFoundPage = () => (
-  window ? (
-    <Layout>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-  ) : (
-    <Loading />
-  )
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
 )
 
 export default NotFoundPage

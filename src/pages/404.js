fcf31b7f1c61a92c1cbb2df497eda4df1a84bc6b
James <james.kiesel@gmail.com>
2020-05-20 10:06:01 +1200

    Render loading until window shows up on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
 import React from "react"
 
+import Loading from "../components/loading"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  window ? (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  ) : (
+    <Loading />
+  )
 )
 
 export default NotFoundPage

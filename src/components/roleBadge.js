3115e3e6258fcb55cfa362b808900af223e3af30
James <james.kiesel@gmail.com>
2020-05-25 18:46:40 +1200

    Move svg to file

--- src/components/roleBadge.js
+++ src/components/roleBadge.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useTranslation } from "react-i18next"
 
 import "../styles/roleBadge.scss"
-import Check from "../images/icons/basic/check-mark.svg"
+import Check from "../images/check-mark.svg"
 
 const RoleBadge = ({ role, checked, onHover, onClick }) => {
   const { t } = useTranslation()
@@ -26,11 +26,7 @@ const RoleBadge = ({ role, checked, onHover, onClick }) => {
         onMouseEnter={() => onHover(role)}
         onMouseLeave={() => onHover(null)}
       >
-        {/* <Check /> */}
-        <svg width="18px" height="18px" viewBox="0 0 18 18">
-          <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
-          <polyline points="1 9 7 14 15 4"></polyline>
-        </svg>
+        <Check />
         {[t(`roles.${role}.icon`), t(`roles.${role}.name`)].join(' ')}
       </div>
     </label>

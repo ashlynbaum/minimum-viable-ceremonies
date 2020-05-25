17f48964fda57d1f34983e33ea986c4a1c977a95
James <james.kiesel@gmail.com>
2020-05-25 23:09:53 +1200

    Position card like editUser

--- src/components/setupCeremony.js
+++ src/components/setupCeremony.js
@@ -15,32 +15,34 @@ const SetupCeremony = ({ onSubmit }) => {
   if (!editingCeremony) { return null }
 
   return (
-    <div className="setup-ceremony flex flex-row">
-      <div className="flex flex-col items-center justify-center">
-        <Card id={editingCeremony.id} namespace="ceremonies" />
-      </div>
-      <div className="flex flex-col">
-        <h3>{t("setup.ceremony.schedule")}</h3>
-        <p>{t("setup.ceremony.scheduleHelptext")}</p>
-        <div className="setup-ceremony-async">
-          {[true, false].map(value => (
-            <label key={value} className="mvc-radio-option flex content-center">
-              <input
-                type="checkbox"
-                name="async"
-                value={value}
-                checked={value === async}
-                onChange={({ target: { checked, value } }) => (
-                  setCeremony(current => ({ ...current, async: value === 'true' }))
-                )}
-              />
-              <div className="mvc-radio-option-label">
-                <Check />
-                <span>{t(`setup.ceremony.${value ? 'async' : 'sync'}`)}</span>
-              </div>
-            </label>
-          ))}
-          <p>{t(`setup.ceremony.${async ? 'async' : 'sync'}Helptext`)}</p>
+    <div className="setup-ceremony">
+      <div className="setup-panel split">
+        <div>
+          <Card id={editingCeremony.id} namespace="ceremonies" />
+        </div>
+        <div>
+          <h3>{t("setup.ceremony.schedule")}</h3>
+          <p>{t("setup.ceremony.scheduleHelptext")}</p>
+          <div className="setup-ceremony-async">
+            {[true, false].map(value => (
+              <label key={value} className="mvc-radio-option flex content-center">
+                <input
+                  type="checkbox"
+                  name="async"
+                  value={value}
+                  checked={value === async}
+                  onChange={({ target: { checked, value } }) => (
+                    setCeremony(current => ({ ...current, async: value === 'true' }))
+                  )}
+                />
+                <div className="mvc-radio-option-label">
+                  <Check />
+                  <span>{t(`setup.ceremony.${value ? 'async' : 'sync'}`)}</span>
+                </div>
+              </label>
+            ))}
+            <p>{t(`setup.ceremony.${async ? 'async' : 'sync'}Helptext`)}</p>
+          </div>
         </div>
       </div>
     </div>

0fb526d5c54617b36ef3a1c498f5ebbb1acae7a5
James <james.kiesel@gmail.com>
2020-05-26 10:20:43 +1200

    Lower case c :/

--- src/components/participant.js
+++ src/components/participant.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react"
 import { useTranslation } from "react-i18next"
 
-import Card from "./Card"
+import Card from "./card"
 import Dropdown from "./dropdown"
 import Context from "../contexts/room"
 import "../styles/participant.scss"

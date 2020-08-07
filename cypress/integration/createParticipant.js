bf1294703fdccb94d63263a55d8a2e15767a7c9f
James <james.kiesel@gmail.com>
2020-08-08 09:40:13 +1200

    Swap out API key

--- cypress/integration/createParticipant.js
+++ cypress/integration/createParticipant.js
@@ -2,7 +2,7 @@ describe('Create participant', () => {
   it('Can create a participant', () => {
     cy.seedRoom('default').then(() => {
       cy.visit('/room/test-room-uuid')
-      cy.contains('What\'s your name?', {timeout: 100_000})
+      cy.contains('What\'s your name?', {timeout: 10_000})
       cy.get('.controls .btn-primary').should('be.disabled')
       cy.get('input[name=username]').type('Barbara Smith')
       cy.get('.controls .btn-primary').should('not.be.disabled')

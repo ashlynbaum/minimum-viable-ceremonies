d5c56704c9bc416b6bd1ecc05927ec0388a405e7
James <james.kiesel@gmail.com>
2020-08-08 00:52:12 +1200

    Move tests into single file

--- cypress/integration/basic.test.js
+++ cypress/integration/basic.test.js
@@ -1,4 +1,21 @@
-describe('Create user', () => {
+describe('Basics', () => {
+  it('Can create a room', () => {
+    cy.visit('/')
+    cy.contains('Get started').click()
+
+    cy.get('.controls .btn-primary').should('be.disabled')
+    cy.get('input[name=name]').type('Test room')
+    cy.get('.controls .btn-primary').should('not.be.disabled')
+    cy.get('.controls .btn-primary').click()
+
+    cy.get('.copy-link').invoke('val').should('match', /http:\/\/localhost:8000\/room/)
+    cy.contains('Create room').click()
+    cy.wait(6000)
+
+    cy.get('.setup-user-name h1').invoke('text').should('match', /What\'s your name/)
+    cy.url().should('include', 'http://localhost:8000/room')
+  })
+
   it('Can create a user', () => {
     cy.seedRoom({
       uuid: 'test-room-uuid',

4703231b44f3ff4d37ac6e019d184fd60b304a0c
James <james.kiesel@gmail.com>
2020-08-09 09:50:39 +1200

    Add freshchat widget

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import FreshChat from "react-freshchat"
 import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react"
 
 import "../styles/layout.scss"
@@ -8,6 +9,11 @@ const Layout = ({ children }) => (
     urlBase: process.env.MATOMO_URL,
     siteId: process.env.MATOMO_SITE_ID
   }) : {}}>
+    <FreshChat
+      host={process.env.FRESHCHAT_HOST}
+      token={process.env.FRESHCHAT_TOKEN}
+      siteId="minimum-viable-ceremonies"
+    />
     <main>{children}</main>
   </MatomoProvider>
 )

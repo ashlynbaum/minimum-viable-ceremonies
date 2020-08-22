d240e045049c686884c464ad14097663a5207158
James <james.kiesel@gmail.com>
2020-08-22 12:30:35 +1200

    Add TODOs for writing integration tests

---
+++ cypress/integration/editRoom.test.js
@@ -0,0 +1,8 @@
+describe('Edit room', () => {
+  it('Can move a ceremony on the board', () => {}) // TODO
+  it('Can copy the shareable link', () => {}) // TODO
+  it('Can collapse and expand the sidebar', () => {}) // TODO
+  it('Can add and remove a sprint week', () => {}) // TODO
+
+  it('Can create a new room', () => {}) // TODO (NB currently broken)
+})
